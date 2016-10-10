2344938e51d83f1d142ae08a5d0a6ff3d04f2bc4
Brian Duffey <briand@mrc-productivity.com>
2016-10-10 17:43:18 -0500

    Bulma changes and beginning of login page

--- imports/ui/Login.vue
+++ imports/ui/Login.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="login">
-    <h1>Login Screen</h1>
-    <a class="button is-primary">Login</a>
+    <h1>{{ mode === 'login' ? 'Login' : 'Register' }}</h1>
+    <br />
+    <label class="label">Email</label>
+    <p class="control">
+      <input class="input" type="email" placeholder="Email Address" />
+    </p>
+    <label class="label">Password</label>
+    <p class="control">
+      <input class="input" type="password" placeholder="Password" />
+    </p>
+    <button class="button is-info">{{ mode === 'login' ? 'Register' : 'Login' }} Instead</button>
+    <button class="button is-primary">{{ mode === 'login' ? 'Login' : 'Register' }}</button>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'login-page',
+    data() {
+      return {
+        mode: 'login'
+      }
+    }
+  }
+</script>
